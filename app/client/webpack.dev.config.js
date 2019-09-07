2144f79e177f9b65e2190a525ffdc7d5c9f6eb5d
Ajit Kumar Satpathy <ajit@xola.com>
2019-09-08 01:16:41 +0530

    Update webpack.dev.config.js

--- app/client/webpack.dev.config.js
+++ app/client/webpack.dev.config.js
@@ -27,7 +27,7 @@ const config = {
     },
     proxy: {
       '/api/**': {
-        target: 'http://localhost:3001',
+        target: 'http://127.0.0.1:3001',
         secure: false
       }
     }
